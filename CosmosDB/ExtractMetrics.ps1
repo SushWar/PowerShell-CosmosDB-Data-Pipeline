9eac71675ea351dae11b7120c91bcc0c51dae158
SushWar <tanwarsushant27@gmail.com>
2024-07-18 21:21:24 +0530

    initialize method

---
+++ CosmosDB/ExtractMetrics.ps1
@@ -0,0 +1,37 @@
+﻿function Get-SystemMetrics {
+    try {
+        # Hostname
+        $deviceName = $env:COMPUTERNAME
+
+        # CPU Usage
+        $cpuUsage = (Get-WmiObject Win32_Processor | Measure-Object -Property LoadPercentage -Average).Average
+
+        # Memory Usage
+        $memory = Get-WmiObject Win32_OperatingSystem | Select-Object FreePhysicalMemory, TotalVisibleMemorySize -First 1
+        $totalMemoryMB = [decimal]$memory.TotalVisibleMemorySize / 1MB
+        $freeMemoryMB = [decimal]$memory.FreePhysicalMemory / 1MB
+        $memoryUsage = ([decimal]$freeMemoryMB / $totalMemoryMB) * 100
+
+        # Disk Usage (adjust for specific drive)
+        $disk = Get-WmiObject Win32_LogicalDisk -Filter "DriveType = 3" | Select-Object FreeSpace, Size -First 1
+        $totalDiskSpaceGB = [decimal]$disk.Size / 1GB
+        $freeDiskSpaceGB = [decimal]$disk.FreeSpace / 1GB
+        $diskUsage = ([decimal]$totalDiskSpaceGB - $freeDiskSpaceGB) / $totalDiskSpaceGB * 100
+
+       # Extract OU
+       $ou = # While I can't disclose the specific method due to privacy concerns, put you method which typically extracts OU values from devices?
+
+        return [PSCustomObject]@{
+            Timestamp = Get-Date
+            device = $deviceName
+            ou = $ou
+            CPUUsage = $cpuUsage
+            MemoryUsage = $memoryUsage
+            DiskUsage = $diskUsage
+            
+        }
+    } catch [System.Management.ManagementException] {
+        Write-Warning "Error getting system metrics: $($_.Exception)"
+        return $null
+    }
+}
---
+++ CosmosDB/SendDataToCosmosDB.ps1
@@ -0,0 +1,108 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$CosmosDBEndPoint = "Your Cosmos Endpoint"
+$DatabaseId = "Database name"
+$collectionId = "Container name"
+$MasterKey = "Primary key"					# For retrieving Cosmos DB account keys, it's strongly recommended to either use the Az.CosmosDB module with the Get-AzCosmosDBAccountKey cmdlet or leverage Azure Key Vault.
+
+# add necessary assembly
+Add-Type -AssemblyName System.Web
+
+######################################################
+
+# generate authorization key
+Function Generate-MasterKeyAuthorizationSignature
+{
+	[CmdletBinding()]
+	Param
+	(
+		[Parameter(Mandatory=$true)][String]$verb,
+		[Parameter(Mandatory=$true)][String]$resourceLink,
+		[Parameter(Mandatory=$true)][String]$resourceType,
+		[Parameter(Mandatory=$true)][String]$dateTime,
+		[Parameter(Mandatory=$true)][String]$key,
+		[Parameter(Mandatory=$true)][String]$keyType,
+		[Parameter(Mandatory=$true)][String]$tokenVersion
+	)
+
+	$hmacSha256 = New-Object System.Security.Cryptography.HMACSHA256
+	$hmacSha256.Key = [System.Convert]::FromBase64String($key)
+
+	$payLoad = "$($verb.ToLowerInvariant())`n$($resourceType.ToLowerInvariant())`n$resourceLink`n$($dateTime.ToLowerInvariant())`n`n"
+	$hashPayLoad = $hmacSha256.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($payLoad))
+	$signature = [System.Convert]::ToBase64String($hashPayLoad);
+
+	[System.Web.HttpUtility]::UrlEncode("type=$keyType&ver=$tokenVersion&sig=$signature")
+}
+
+######################################################
+Function Post-CosmosDb
+{
+	[CmdletBinding()]
+	Param
+	(
+		[Parameter(Mandatory=$true)][String]$EndPoint,
+		[Parameter(Mandatory=$true)][String]$DataBaseId,
+		[Parameter(Mandatory=$true)][String]$CollectionId,
+		[Parameter(Mandatory=$true)][String]$MasterKey,
+		[Parameter(Mandatory=$true)][String]$JSON
+	)
+	try {
+		$Verb = "POST"
+		$ResourceType = "docs";
+		$ResourceLink = "dbs/$DatabaseId/colls/$CollectionId"
+		$partitionkey = "[""$(($JSON |ConvertFrom-Json).ou)""]"					# My data is partitioned by Organizational Unit (OU). To extract the OU value, please provide the partition key you'd like to use.
+		
+		Write-Host $partitionkey
+
+		$dateTime = [DateTime]::UtcNow.ToString("r")
+		$authHeader = Generate-MasterKeyAuthorizationSignature -verb $Verb -resourceLink $ResourceLink -resourceType $ResourceType -key $MasterKey -keyType "master" -tokenVersion "1.0" -dateTime $dateTime
+		$header = @{authorization=$authHeader;"x-ms-documentdb-partitionkey"=$partitionkey;"x-ms-version"="2018-12-31";"x-ms-date"=$dateTime}
+		$contentType= "application/json"
+		$queryUri = "$EndPoint$ResourceLink/docs"
+		
+		
+		#Convert to UTF8 for special characters
+		$defaultEncoding = [System.Text.Encoding]::GetEncoding('ISO-8859-1')
+		$utf8Bytes = [System.Text.Encoding]::UTf8.GetBytes($JSON)
+		$bodydecoded = $defaultEncoding.GetString($utf8bytes)
+		
+		
+		Invoke-RestMethod -Method $Verb -ContentType $contentType -Uri $queryUri -Headers $header -Body $bodydecoded -ErrorAction SilentlyContinue
+   	} 
+   catch {
+		Write-Host "error block"
+		return $_.Exception.Response.StatusCode.value__ 
+   	}
+    
+	
+}
+
+
+
+
+##			Start JOB				##
+
+
+$metricJob = Start-Job -ScriptBlock {Import-Module ".\ExtractMetrics.ps1"; Get-SystemMetrics}
+
+$metrics = Receive-Job $metricJob -Wait
+
+if ($metricJob.State -eq "Completed") {
+    $Data = New-Object System.Object
+    $Data | Add-Member -MemberType NoteProperty -Name "id" -Value $(New-GUID) -Force
+    $Data | Add-Member -MemberType NoteProperty -Name "ou" -Value $metrics.ou  -Force
+    $Data | Add-Member -MemberType NoteProperty -Name "device" -Value $metrics.device  -Force
+    $Data | Add-Member -MemberType NoteProperty -Name "cpu" -Value $metrics.CPUUsage -Force
+    $Data | Add-Member -MemberType NoteProperty -Name "memory" -Value $metrics.MemoryUsage -Force
+    $Datajson = $Data | ConvertTo-Json
+    $DeviceResult=Post-CosmosDb -EndPoint $CosmosDBEndPoint -DataBaseId $DataBaseId -CollectionId $collectionId -MasterKey $MasterKey -JSON $Datajson
+    Write-Host "Metrics sent to Cosmos DB successfully." $Datajson
+} else {
+    Write-Warning "Metrics extraction failed. Exit code: $($metricJob.Error)"
+}
+
+Remove-Job $metricJob
+
+##			End JOB				##
+
+
